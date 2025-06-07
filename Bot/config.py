9746fc1a15f2b00fb62b89d98e72bdb66e6486f5
ber120 <babluverma335051@gmail.com>
2025-06-08 02:05:13 +0530

    Update config.py

--- Bot/config.py
+++ Bot/config.py
@@ -11,16 +11,16 @@ load_dotenv()
 class Config(object):
 
     # Your API HASH
-    API_HASH = os.environ.get('API_HASH')
+    API_HASH = os.environ.get('ca156f58ad03bb5e350c855bb82e9991')
 
     # Your API ID
-    APP_ID = int(os.environ.get('APP_ID'))
+    APP_ID = int(os.environ.get('24731191'))
 
     # Your Bot Token
-    BOT_TOKEN = os.environ.get('BOT_TOKEN')
+    BOT_TOKEN = os.environ.get('7366561149:AAFLtJcH211zPAbXvBdTdo-NpU9qBZe64Dg')
 
     # Your Telegram ID (optional)
-    OWNER_ID = os.environ.get('OWNER_ID')
+    OWNER_ID = os.environ.get('5435086550')
 
     # Upload method (default to False)
     AS_ZIP = bool(strtobool(os.environ.get('AS_ZIP', 'False')))
